534f91f60f4c1cf3ddf96741085f22f9da04eed8
Guilherme-Jardim <guilhermejardimj@gmail.com>
2024-05-10 13:44:53 -0300

    validate-check-in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -12,11 +12,11 @@ describe('Validate Check-in Use Case', () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
 
-    // vi.useFakeTimers()
+    vi.useFakeTimers()
   })
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check in', async () => {
@@ -39,5 +39,24 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'Inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutos of its creation', async () => {
+    vi.setSystemTime(new Date(2024, 4, 10, 13, 35))
+
+    const CreatedCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: CreatedCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
